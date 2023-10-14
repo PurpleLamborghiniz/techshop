5d813638bd10ecea75f4d6de496d59075a07bc2c
PurpleLamborghiniz <98333360+PurpleLamborghiniz@users.noreply.github.com>
2023-10-14 11:05:21 +0300

    Add files via upload

---
+++ TechnicalShop/Components/ItemUserControl.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TechnicalShop.Baseы;
+
+namespace TechnicalShop.Components
+{
+    /// <summary>
+    /// Логика взаимодействия для ItemUserControl.xaml
+    /// </summary>
+    public partial class ItemUserControl : UserControl
+    {
+        public ItemUserControl(Product product)
+        {
+            InitializeComponent();
+            //if(product.MainImagePath == null)
+
+            MainImage.Source = new BitmapImage(new Uri(@"/Resources/NoImage.png", UriKind.Relative));
+            TitleTb.Text = product.Title;
+            NewPrice.Text = product.CostDiscount;
+            OldPrice.Visibility = product.CostVisibility;
+            EvalutionTb.Text = product.OverideFeedback.ToString("N2");
+            OldPrice.Text = product.Cost.ToString("N0");
+            BasketBt.Source = new BitmapImage(new Uri(@"/Resources/Basket.png", UriKind.Relative));
+            LikeBt.Source = new BitmapImage(new Uri(@"/Resources/Heart.png", UriKind.Relative));
+            StatisticBt.Source = new BitmapImage(new Uri(@"/Resources/Statistik.png", UriKind.Relative));
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TechnicalShop/Databases/Item.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TechnicalShop.Baseы
+{
+    public partial class Product
+    {
+        public string CostDiscount
+        {
+            get
+            {
+                if (Discount == 0)
+                    return Cost.ToString("N0") + " ₽";
+                else
+                    return (Cost - (Cost * (decimal)Discount / 100)).ToString("N0") + " ₽  ";
+            }
+        }
+        public Visibility CostVisibility
+        {
+            get
+            {
+                if (Discount == 0)
+                    return Visibility.Collapsed;
+                else
+                    return Visibility.Visible;
+            }
+        }
+
+        public double OverideFeedback
+        {
+            get
+            { 
+                double sum = 0;
+
+                foreach(var item in Feedback)
+                {
+                    sum += item.Evaluation;
+
+                }
+                return sum / Feedback.Count;
+
+            }
+        }
+
+    }
+}
